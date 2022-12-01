391d60705a31c81ff80ffd2b0b4ffb0b1af4413a
Kyle Coffey <me@phytolizer.dev>
2022-12-01 13:23:00 -0600

    refactor

--- src/aoc_2022/dec_01.clj
+++ src/aoc_2022/dec_01.clj
@@ -11,17 +11,18 @@
           maxes
           (cons x (remove #{min-max} maxes)))))))
 
+(defn- iter [numbers count maxes keep]
+  (if (empty? numbers)
+    (apply + (if (> count 0)
+               (insert-max maxes count keep)
+               maxes))
+    (let [number (first numbers)]
+      (if (empty? number)
+        (recur (rest numbers) 0 (insert-max maxes count keep) keep)
+        (recur (rest numbers) (+ count (Integer/parseInt number)) maxes keep)))))
+
 (defn run [input part]
   (with-open [rdr (reader input)]
     (let [numbers (line-seq rdr)
-          maxes-to-keep (if (= part 1) 1 3)
-          iter (fn [numbers count maxes]
-                 (if (empty? numbers)
-                   (apply + (if (> count 0)
-                              (insert-max maxes count maxes-to-keep)
-                              maxes))
-                   (let [number (first numbers)]
-                     (if (empty? number)
-                       (recur (rest numbers) 0 (insert-max maxes count maxes-to-keep))
-                       (recur (rest numbers) (+ count (Integer/parseInt number)) maxes)))))]
-      (iter numbers 0 nil))))
+          maxes-to-keep (if (= part 1) 1 3)]
+      (iter numbers 0 nil maxes-to-keep))))
