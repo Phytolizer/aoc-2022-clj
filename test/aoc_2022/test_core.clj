85b38cc99f493563aa3946fc65da89a5ebeee247
Kyle Coffey <me@phytolizer.dev>
2022-12-05 11:00:24 -0600

    comment the macro

--- test/aoc_2022/test_core.clj
+++ test/aoc_2022/test_core.clj
@@ -3,15 +3,19 @@
 
 (defmacro advent-test [day expected-arr]
   (list 'do
+        ; require the appropriate day's namespace
         `(require (quote [~(symbol (format "aoc-2022.dec-%02d" day)) :as ~(symbol "day")]))
         (apply (partial list
+                        ; outer deftest
                         'clojure.test/deftest
                         (symbol (format "test-dec%02d" day)))
+               ; test body = 4 subtests
                (for [i (range 0 4)]
                  (let [input (nth ["simple" "full"] (mod i 2))
                        part (inc (quot i 2))
                        expected (nth expected-arr i)
                        run (symbol "day" "run")]
+                   ; each test is defined here
                    `(testing ~(format "part %d, %s input" part input)
                       (is (= ~expected
                              (~run ~(format "inputs/dec-%02d-%s.txt" day input)
