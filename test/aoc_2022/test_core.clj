e762fa5f697abb9d19415518dfb96dd80c896e63
Kyle Coffey <me@phytolizer.dev>
2022-12-05 13:47:53 -0600

    simplify? macro

--- test/aoc_2022/test_core.clj
+++ test/aoc_2022/test_core.clj
@@ -1,23 +1,15 @@
 (ns aoc-2022.test-core
-  (:require [clojure.test :refer [is testing]]))
+  (:require [clojure.test :refer [deftest is testing]]))
 
 (defmacro advent-test [day expected-arr]
-  (let [day-ns (format "aoc-2022.dec-%02d" day)]
-    (list 'do
-          ; require the appropriate day's namespace
-          `(require (quote ~(symbol day-ns)))
-          (apply (partial list
-                          ; outer deftest
-                          'clojure.test/deftest
-                          (symbol (format "test-dec%02d" day)))
-                 ; test body = 4 subtests
-                 (for [i (range 0 4)]
-                   (let [input (nth ["simple" "full"] (mod i 2))
-                         part (inc (quot i 2))
-                         expected (nth expected-arr i)
-                         run (symbol day-ns "run")]
-                     ; each test is defined here
-                     `(testing ~(format "part %d, %s input" part input)
-                        (is (= ~expected
-                               (~run ~(format "inputs/dec-%02d-%s.txt" day input)
-                                     ~part))))))))))
+  `(deftest ~(symbol (format "test-dec%02d" day))
+     (require '~(symbol (format "aoc-2022.dec-%02d" day)))
+     ~@(for [i (range 0 4)]
+         (let [input (nth ["simple" "full"] (mod i 2))
+               part (inc (quot i 2))
+               expected (nth expected-arr i)
+               run `(ns-resolve '~(symbol (format "aoc-2022.dec-%02d" day)) '~'run)]
+           `(testing ~(format "part %d, %s input" part input)
+              (is (= ~expected
+                     (~run ~(format "inputs/dec-%02d-%s.txt" day input)
+                           ~part))))))))
